3190f7e6f5f069b5458f0cfdfa953253c6de1e46
RyoCa <ryo.ca.dev@gmail.com>
2024-05-27 23:58:13 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:roulette/roulette.dart';
+import 'package:roulette_app/components/arrow.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Roulette',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class MyRoulette extends StatelessWidget {
+  const MyRoulette({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final RouletteController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        SizedBox(
+          width: 260,
+          height: 260,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Roulette(
+              controller: controller,
+              style: const RouletteStyle(
+                dividerThickness: 0.0,
+                dividerColor: Colors.black,
+                centerStickSizePercent: 0.05,
+                centerStickerColor: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        const Arrow(),
+      ],
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // static final _random = Random();
+
+  late RouletteController _controller;
+
+  final colors = <Color>[
+    Colors.red.withAlpha(50),
+    Colors.green.withAlpha(30),
+    Colors.blue.withAlpha(70),
+    Colors.amber.withAlpha(50),
+    Colors.indigo.withAlpha(70),
+  ];
+
+  final List<String> texts = [
+    'Aがすべて',
+    'Bがすべて',
+    'AとBで半分',
+    'Aが多め',
+    'Bが多め',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = RouletteController(
+      vsync: this,
+      group: RouletteGroup.uniform(colors.length,
+          colorBuilder: (index) => colors[index],
+          textBuilder: (index) => texts[index],
+          textStyleBuilder: (index) => const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Roulette'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.pink.withOpacity(0.1),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MyRoulette(controller: _controller),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _controller.rollTo(
+          3,
+          clockwise: true,
+          // offset: _random.nextDouble(), // ランダムは消す
+          offset: 3.2, // Aがすべてを指すようにする
+        ),
+        child: const Icon(Icons.refresh_rounded),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
