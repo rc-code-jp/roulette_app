d3b48b1de6b0c024f9a09ae18459d6fd278817f6
rc-code-jp <rc-code-jp@users.noreply.github.com>
2024-05-28 14:42:28 +0000

    deploy: 897ce5e57210afd50501b9f84af6f3dfb477353a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2567522549"
+    serviceWorkerVersion: "2230733610"
   }
 });
